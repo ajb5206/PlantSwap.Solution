bf551fdbe5f6fdd5d8dd1c07eb48b93ef1b15e3c
Shane Graff <copellius@gmail.com>
2021-11-21 16:06:51 -0800

    Initial commit
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>
    Co-authored-by: Tim Roth <timdroth@gmail.com>
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

---
+++ PlantSwap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlantSwap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
