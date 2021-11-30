6b742446e47b5784f57b7a6f14bd7822a528d798
Shane Graff <copellius@gmail.com>
2021-11-29 19:21:48 -0800

    Add loginviewmodel

---
+++ PlantSwap/ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlantSwap.ViewModels
+{
+  public class LoginViewModel
+  {
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserName { get; set; }
+  }
+}
