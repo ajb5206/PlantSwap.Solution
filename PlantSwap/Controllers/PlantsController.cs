82cbacff405736f2bd274aec8843cbdcfae58d45
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-12-06 19:13:57 -0800

    Update the model to include the reciprocal plants of the offers and requests
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- PlantSwap/Controllers/PlantsController.cs
+++ PlantSwap/Controllers/PlantsController.cs
@@ -59,7 +59,7 @@ namespace PlantSwap.Controllers
     //The id passed into the Details action method comes from the Razor Action Link on line 17 of Views/Plants/Index (or line 25 of Views/Home/Index). That action link is inside a loop through the Index's model so it is only interacting with a single plant at a time (the plant of that iteration). The action link grabs the id from the iteration of the Index's model.
     public ActionResult Details(int id)
     {
-      //Gives a list of Plant objects from the database (but not its associated offers or requests)
+      //Gives a list of Plant objects from the plants table of database (but not its associated offers or requests)
       Plant thisPlant = _db.Plants
           //Loads the OfferJoinEntity property of each Plant (this is just a collection of join entities, not the actual Trader or reciprocal plants)
           .Include(plant => plant.OfferJoinEntity)
@@ -72,14 +72,46 @@ namespace PlantSwap.Controllers
           //Specifies which Plant from the database we're working with (i.e. for which plant we're displaying the details)
           .FirstOrDefault(plant => plant.PlantId == id);
 
+      //Gives a list of Offer objects from the Offers table of the database
+      var AcceptablePlantList = _db.Offers
+      //Narrows the list to only the offers that have the property PlantId that is equal to the value of the PlantId of thisPlant (the plant whose details we are displaying as selected in lines 63-73)
+        .Include(offer => offer.PlantId == thisPlant.PlantId);
 
-        //Experiment that doesn't yet work
-        Plant PlantsWillAcceptForAnOffer = _db.Plants
-          .Include(plant => plant.OfferJoinEntity)
-          //.Any(plant => plant.CommonName == WillAcceptPlantId);
-          .Any(plant => plant.PlantId == OfferJoinEntity.WillAcceptPlantId);
+      //Creates a new empty dictionary
+      Dictionary<int, Object> AcceptablePlants = new Dictionary<int, Object> {};
+
+      //Iterates through the list of offer created in lines 76-78. Reminder: That is a list of offers that have the property PlantId that is equal to the value of the PlantId of thisPlant (the plant whose details we are displaying as selected in lines 63-73)
+      //Each time through the loop it creates a list of all the plant objects in the Plants table. Then it narrows down the list to the only plant that has the PlantId property that matches the WillAcceptPlantId property of the offer object currently being iterated through. We store this plant in the variable acceptablePlant.
+      //Then before completing the iteration, it adds to the dictionary an entry where the dictionary entry key is the int WillAcceptPlantId property of the offer and the dictionary entry value is the acceptablePlant from the step above.
+      foreach (Offer offer in AcceptablePlantList)
+      {
+        Plant acceptablePlant = _db.Plants
+          .FirstOrDefault(plant => plant.PlantId == offer.WillAcceptPlantId);
+
+        AcceptablePlants.Add(offer.WillAcceptPlantId, acceptablePlant);
+      }
+
+      //To that same dictionary we add the key of `0` and the value of `thisPlant` (which is the plant whose details are being displayed - see lines 63-73)
+      AcceptablePlants.Add(0, thisPlant);
+        
+
+
+
+        //What we need to do: Like above 
+          //Create a list of Plant objects from the database
+          //Load the OfferJoinEntity property for each Plant
+          //Load the Trader associated with the OfferJoinEntity
+          //Load the RequestJoinEntity property for each Plant
+          //Load the Trader associated with the RequestJoinEntity
+          //Specify which Plant from the database we're working with
+        //New and additional from above
+          //For the plant we're working with, iterate through it's OfferJoinEntity and pull out all the WillAcceptPlantIds
+        
+        //Or go right to the offers Table, Iterate through and pull out entries with PlantId as that thisPlant.PlantId and grab from those entries: int WillAcceptPlantId
+        //For each of those id's, produce a link to the details view for the actual plant object it represents in _db.Plants
+        //Do the same for the Requests Table and HaveToOfferPlantId
           
-      return View(thisPlant);
+      return View(AcceptablePlants);
     }
 
 //Add User Authorization
