918b777dda89b19e04deaad05294a9c075a9eda2
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-12-06 20:42:12 -0800

    Add migration files
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- PlantSwap/Migrations/20211207043949_UpdateRequestClass.Designer.cs
+++ PlantSwap/Migrations/20211207043949_UpdateRequestClass.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlantSwap.Models;
 
 namespace PlantSwap.Migrations
 {
     [DbContext(typeof(PlantSwapContext))]
-    partial class PlantSwapContextModelSnapshot : ModelSnapshot
+    [Migration("20211207043949_UpdateRequestClass")]
+    partial class UpdateRequestClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
