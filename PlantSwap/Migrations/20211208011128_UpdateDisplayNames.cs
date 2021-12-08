cc453b3eb8e072474a21b7794f35c922612e815a
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-12-07 17:12:23 -0800

    Add migtation files

---
+++ PlantSwap/Migrations/20211208011128_UpdateDisplayNames.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlantSwap.Migrations
+{
+    public partial class UpdateDisplayNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
